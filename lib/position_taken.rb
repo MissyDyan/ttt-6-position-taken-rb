eee575eac145b592c0269fee5181ee0a1ce05854
Diane Nguyen <diane.nguyen@gmail.com>
2021-07-01 21:47:25 -0700

    Tic Tac Toe Position Taken assignment completd.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, player_input)
+    if board[player_input] == " "
+        return false
+    elsif board[player_input] == "" || board[player_input] == nil
+        return false
+    else board[player_input] == "X" || board[player_input] == "O"
+        return true
+    end
+end
